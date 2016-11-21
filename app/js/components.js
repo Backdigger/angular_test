36b59efb36f8f20522166fdc143f26b9ea4bb29c
hlebedzeva <hlebedzeva@scnsoft.com>
2016-11-21 10:53:25 +0300

    components modification

--- app/js/components.js
+++ app/js/components.js
@@ -1,17 +1,13 @@
 app.component('notesList', {
+    template: `<div ng-controller="customersCtrl" class="row">
+                    <div  ng-repeat="note in data" ><notes-item></notes-item></div></div>`,
 
-    bindings: {
-        notes: '='
-    }
+    bindings: { note: '=' }
 })
     .component('notesItem', {
-        template: ` <notes-item ng-controller="customersCtrl" class="row"><notes-item  ng-repeat="note in data"
-'style="background-color: {{note.color}}"
-'class="col-xs-4 note-card"><div class="col-xs-12 title">{{note.title}}</div><div class="col-xs-12 value">{{note.value}}</div></notes-item>
-        </div>`,
-        bindings: { note: '=&' },
-        require: {
-            parent: 'notesList'
-        }
+        template: ` <div style="background-color: {{note.color}}" class="col-xs-4 note-card">
+                    <div class="col-xs-12 title">{{note.title}}</div>
+                    <div class="col-xs-12 value">{{note.value}}</div></div>`,
 
+        bindings: { note: '=' }
     });
