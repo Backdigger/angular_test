139f67ef8837bbc832e719146fff43f6acd2606b
hlebedzeva <hlebedzeva@scnsoft.com>
2016-11-23 12:38:41 +0300

    components fixed by passing  scope

--- app/js/components.js
+++ app/js/components.js
@@ -2,16 +2,16 @@
     angular
         .module('myApp')
         .component('notesList', {
-            template: `<div  ng-repeat="note in $ctrl.notes">{{note}}
-                        <notes-item note="note"></notes-item></div>`,
-            bindings: { notes: '<' }
+            template: `<div  ng-repeat="note in $ctrl.notes"><notes-item note="note"></notes-item>
+                        </div>`,
+            bindings: { notes: '=' }
 
         })
         .component('notesItem', {
-            template: ` <div style="background-color: {{note.color}}" class="col-xs-4 note">
-                    <div class="col-xs-12 title">{{note.title}}</div>
-                    <div class="col-xs-12 value">{{note.value}}</div></div>`,
-            bindings: { note: '<' }
+            template: ` <div style="background-color: {{$parent.note.color}}" class="col-xs-4 note">
+                    <div class="col-xs-12 title">{{$parent.note.title}}</div>
+                    <div class="col-xs-12 value">{{$parent.note.value}}</div></div>`,
+            bindings: { note: '=' }
 
         });
 
