a55655e1fba31d38ba99220afc89609997c4bedf
hlebedzeva <hlebedzeva@scnsoft.com>
2016-12-06 13:02:26 +0300

    ng-show fixed

--- app/js/components.js
+++ app/js/components.js
@@ -1,20 +1,21 @@
 (function () {
     function NoteListController($scope, $element, $attrs) {
-        let ctrl = this;
 
-        ctrl.colors = ['#ff66ff', '#0033cc', '#669900', '#cc0066', '#99ccff', 'white'];
 
-        ctrl.createNote = function (note) {
+        this.colors = ['#ff66ff', '#0033cc', '#669900', '#cc0066', '#99ccff', 'white'];
+        this.isColorListShow = false;
+
+        this.createNote = (note) => {
             const { title, value, color } = note;
 
             if (title && value) {
-                ctrl.notes.push({ title, value, color });
+                this.notes.push({ title, value, color });
             }
-            ctrl.resetNote();
+            this.resetNote();
         };
 
 
-        ctrl.resetNote = function (note) {
+        this.resetNote = (note) => {
             note = {
                 title: '',
                 value: '',
@@ -22,15 +23,16 @@
             };
         };
 
-        ctrl.deleteNote = function (note) {
-            let idx = ctrl.notes.indexOf(note);
+        this.deleteNote = (note) => {
+            let idx = this.notes.indexOf(note);
 
             if (idx >= 0) {
-                ctrl.notes.splice(idx, 1);
+                this.notes.splice(idx, 1);
             }
         };
-        ctrl.expand = function (color) {
-            color.show = !color.show;
+
+        this.toggleColorList =  ()  => {
+            this.isColorListShow = !this.isColorListShow;
         };
     }
     function NoteDetailController() {
@@ -56,9 +58,9 @@
                        <input placeholder="Take a note..." name="newNoteValue" class="col-xs-10" type="text" ng-model="note.value">
                        <div class="actions col-xs-12 row">
                        <div class="col-xs-3">
-                       <div class="color-selector" colors="#ff66ff, #0033cc, #669900, #cc0066, #99ccff, white" ng-model="note.color">
-                       <i class="material-icons icon"  ng-click="$ctrl.expand(color)">color_lens</i>
-                       <ul class="selector" ng-show="color.show">
+                       <div class="color-selector" colors="#ff66ff, #0033cc, #669900, #cc0066, #99ccff, white" ng-model="color">
+                       <i class="material-icons icon"  ng-click="$ctrl.toggleColorList()">color_lens</i>
+                       <ul class="selector" ng-show="$ctrl.isColorListShow">
                        <li ng-repeat="color in $ctrl.colors" >
                        <div  class="color" style="background-color: {{color}}">
                        </div>
@@ -77,7 +79,7 @@
 
         })
         .component('notesList', {
-            template: `<div  ng-repeat="note in $ctrl.notes"><notes-item note="note" on-click="$ctrl.deleteNote(note)"  on-reset="$ctrl.updateNote()"><div ng-style ="{'background-color': note.color}" class="col-xs-4 note">
+            template: `<div  ng-repeat="note in $ctrl.notes"><notes-item note="note" on-delete="$ctrl.deleteNote(note)"  on-update="$ctrl.updateNote()"><div ng-style ="{'background-color': note.color}" class="col-xs-4 note">
                        <div class="col-xs-12 title">{{note.title}}</div>
                        <div class="col-xs-12 value">{{note.value}}</div></div></notes-item>
                        </div>`,
