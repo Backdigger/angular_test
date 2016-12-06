6c1c98a6884fe98adcfec9fe62d6907e3aea3e2f
hlebedzeva <hlebedzeva@scnsoft.com>
2016-12-06 16:18:58 +0300

    delete function fixed

--- app/js/components.js
+++ app/js/components.js
@@ -1,12 +1,17 @@
 (function () {
     function NoteListController($scope, $element, $attrs) {
 
-
         this.colors = ['#ff66ff', '#0033cc', '#669900', '#cc0066', '#99ccff', 'white'];
+        this.newNote = {
+            title: '',
+            value: '',
+            color: 'white'
+        };
         this.isColorListShow = false;
 
         this.createNote = (note) => {
-            const { title, value, color } = note;
+            this.newNote = note;
+            const { title, value, color } = this.newNote;
 
             if (title && value) {
                 this.notes.push({ title, value, color });
@@ -15,16 +20,16 @@
         };
 
 
-        this.resetNote = (note) => {
-            note = {
+        this.resetNote = (newNote) => {
+            newNote = {
                 title: '',
                 value: '',
                 color: 'white'
             };
         };
 
-        this.deleteNote = (note) => {
-            let idx = this.notes.indexOf(note);
+        this.deleteNote = (newNote) => {
+            let idx = this.notes.indexOf(newNote.note);
 
             if (idx >= 0) {
                 this.notes.splice(idx, 1);
@@ -34,16 +39,20 @@
         this.toggleColorList =  ()  => {
             this.isColorListShow = !this.isColorListShow;
         };
+
+        this.selectColor = (color) => {
+            this.toggleColorList();
+            this.newNote.color =  color;
+        };
     }
     function NoteDetailController() {
-        var ctrl = this;
 
-        ctrl.delete = function () {
-            ctrl.onDelete({ note: ctrl.note });
+        this.delete = () => {
+            this.onDelete({ note: this.note });
         };
 
-        ctrl.update = function (prop, value) {
-            ctrl.onUpdate({ note: ctrl.note, prop: prop, value: value });
+        this.update = (prop, value) => {
+            this.onUpdate({ note: this.note, prop: prop, value: value });
         };
 
     }
@@ -52,36 +61,37 @@
         .module('myApp')
 
         .component('noteCreator', {
-            template: `<div ng-style ="{'background-color': note.color}" >
-                       <form ng-submit="$ctrl.createNote(note)">
-                       <input placeholder="Title" name="newNoteTitle" class="col-xs-10 title" type="text" ng-model="note.title">
-                       <input placeholder="Take a note..." name="newNoteValue" class="col-xs-10" type="text" ng-model="note.value">
+            template: `<div ng-style ="{'background-color': this.newNote.color}" >
+                       <form ng-submit="$ctrl.createNote(newNote)">
+                       <input placeholder="Title" name="newNoteTitle" class="col-xs-10 title" type="text" ng-model="newNote.title">
+                       <input placeholder="Take a note..." name="newNoteValue" class="col-xs-10" type="text" ng-model="newNote.value">
                        <div class="actions col-xs-12 row">
                        <div class="col-xs-3">
-                       <div class="color-selector" colors="#ff66ff, #0033cc, #669900, #cc0066, #99ccff, white" ng-model="color">
+                       <div class="color-selector" note="note">
                        <i class="material-icons icon"  ng-click="$ctrl.toggleColorList()">color_lens</i>
                        <ul class="selector" ng-show="$ctrl.isColorListShow">
                        <li ng-repeat="color in $ctrl.colors" >
-                       <div  class="color" style="background-color: {{color}}">
+                       <div  note="note" ng-click="$ctrl.selectColor(color)" ng-model="newNote.color" class="color" style="background-color: {{color}}">
                        </div>
                        </li>                       
                        </ul>
                        </div>
                        </div>
                        </div>
-                       <button ng-click="$ctrl.addNote()" type="submit" class="btn-light">Done</button>
+                       <button type="submit" class="btn-submit">Done</button>
                        </form>
                        </div>`,
             bindings: { notes: '<',
-                colors: '@',
+
+                color: '<',
                 onSubmit: '&' },
             controller: NoteListController
 
         })
         .component('notesList', {
-            template: `<div  ng-repeat="note in $ctrl.notes"><notes-item note="note" on-delete="$ctrl.deleteNote(note)"  on-update="$ctrl.updateNote()"><div ng-style ="{'background-color': note.color}" class="col-xs-4 note">
+            template: `<div  ng-repeat="note in $ctrl.notes"><notes-item note="note"   on-update="$ctrl.updateNote()"><div ng-style ="{'background-color': note.color}" class="col-xs-4 note">
                        <div class="col-xs-12 title">{{note.title}}</div>
-                       <div class="col-xs-12 value">{{note.value}}</div></div></notes-item>
+                       <div class="col-xs-12 value">{{note.value}}</div><button class="btn-delete" ng-click="$ctrl.deleteNote({note: note})">Delete note</button></div></notes-item>
                        </div>`,
             bindings: { notes: '<' },
             controller: NoteListController
@@ -91,8 +101,7 @@
         .component('notesItem', {
             bindings: { note: '<',
                 onDelete: '&',
-                onUpdate: '&',
-                onSubmit: '&'
+                onUpdate: '&'
             },
             controller: NoteDetailController
 
@@ -100,7 +109,5 @@
         });
 
 })();
-
-
 
 
